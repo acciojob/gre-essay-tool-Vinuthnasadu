4abcbd79b57e9c89dd364d14a05402529567d47a
Vinuthnasadu <lakshmivinuthna2411@gmail.com>
2022-12-28 13:09:31 +0530

    coded

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your code here
+let text = document.getElementById("evaluatedText");
+let count = document.getElementById("wordCount");
+let cnt=0;
+evaluatedText.addEventListener('keyup', (e) => {
+
+    if(e.key == " "){
+      cnt=cnt+1;
+      wordCount.innerHTML = cnt;
+      console.log(cnt);
+    }
+    else{
+        
+    }
+})
